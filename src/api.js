8506e3cd6cfbd3ec3b000046fa4ae825230125c6
ayaOaya <alyaauaacn@gmail.com>
2023-05-22 21:14:03 +0200

    changing key

---
+++ src/api.js
@@ -0,0 +1,15 @@
+const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8c8b7d7e85mshfa26dc8805ec449p155a61jsnea1017fbea53',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+  
+
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "9a1e27e9e7f9019124c7f056d533bc2a"; // enter your key from openweather API
