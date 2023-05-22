6e744baec0611b0e64b35629017ebd377b020bde
ayaOaya <alyaauaacn@gmail.com>
2023-05-22 19:39:32 +0200

    adding additional changes

--- api.jsx
+++ api.jsx
@@ -8,4 +8,4 @@ export const geoApiOptions = {
   export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
   
   export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-  export const WEATHER_API_KEY = ""; // enter your key from openweather API
+  export const WEATHER_API_KEY = "0a08e2f1ded816569c521592591a901a"; // enter your key from openweather API
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
 import Forecast from "./components/forecast/forecast";
-import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+/* import { WEATHER_API_URL, WEATHER_API_KEY } from "./api"; */
+import { WEATHER_API_KEY, WEATHER_API_URL } from "../api";
 import "./App.css";
 
 function App() {
